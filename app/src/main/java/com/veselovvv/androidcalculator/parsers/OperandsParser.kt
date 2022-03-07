33dcf969dc6a1432c2b6bf1cf922bcc5e76dbead
veselovvv <vycheslav5veselov@gmail.com>
2022-03-07 18:00:58 +0200

    Initial commit

---
+++ app/src/main/java/com/veselovvv/androidcalculator/parsers/OperandsParser.kt
@@ -0,0 +1,36 @@
+package com.veselovvv.androidcalculator.parsers
+
+interface OperandsParser {
+    fun parseOperands(expression: String): Array<String>
+    fun isOperand(element: Char): Boolean
+
+    class Base : OperandsParser {
+        // Находит операнды в строке:
+        override fun parseOperands(expression: String): Array<String> {
+            var indexOperand = 0
+            var countOperand = 0
+
+            // Количество операндов для создания массива:
+            expression.forEach { element ->
+                if (isOperand(element)) countOperand++
+            }
+
+            val operandsArray = Array(countOperand){""}
+
+            // Поиск операндов:
+            expression.forEach { element ->
+                // Если в строке выражения есть операнд, добавить его в массив операндов:
+                if (isOperand(element)) {
+                    operandsArray[indexOperand] = element.toString()
+                    indexOperand++
+                }
+            }
+
+            return operandsArray
+        }
+
+        // Возвращает true если элемент - операнд:
+        override fun isOperand(element: Char) =
+            (element == '+') || (element == '-') || (element == '*') || (element == '/')
+    }
+}
