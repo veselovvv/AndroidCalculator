294bb59b6ab3a2d25760c1ae1a0b56ed7c780ff8
veselovvv <vycheslav5veselov@gmail.com>
2024-08-23 17:18:14 +0300

    Added Main Page object

---
+++ app/src/androidTest/java/com/veselovvv/androidcalculator/MainPage.kt
@@ -0,0 +1,20 @@
+package com.veselovvv.androidcalculator
+
+class MainPage {
+    private val editTextStateUi = EditTextStateUi()
+    private val resultButtonUi = ButtonUi(buttonId = R.id.result_button)
+    private val clearButtonUi = ButtonUi(buttonId = R.id.clear_button)
+    private val resultTextViewUi = ResultTextViewUi()
+
+    fun checkInitialUiState() {
+        resultButtonUi.checkIsDisplayed()
+        clearButtonUi.checkIsDisplayed()
+        editTextStateUi.checkInitialState()
+        resultTextViewUi.checkInitialState()
+    }
+
+    fun typeInEditText(text: String) = editTextStateUi.typeIn(text = text)
+    fun checkEditTextState(text: String) = editTextStateUi.checkState(text = text)
+    fun clickResultButton() = resultButtonUi.clickButton()
+    fun checkResultTextViewState(text: String) = resultTextViewUi.checkState(text = text)
+}
